1ebb65696eada2ed7ddbec6c5a65d89f56eaf1de
Peter <guan_peter123@yahoo.com>
2023-03-16 14:12:45 -0700

    phase 4 done

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -42,4 +42,30 @@ router.post(
     }
 );
 
+//logout route
+router.delete(
+    '/',
+    (_req, res) => {
+        res.clearCookie('token');
+        return res.json({ message: 'success' });
+    }
+);
+
+router.get(
+    '/',
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        const safeUser = {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        };
+        return res.json({
+          user: safeUser
+        });
+      } else return res.json({ user: null });
+    }
+  );
+
 module.exports = router;
