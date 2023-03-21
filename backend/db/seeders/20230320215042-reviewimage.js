05d71f61be89a8baa3b99c5f0cf2044ece837b2c
Peter <guan_peter123@yahoo.com>
2023-03-20 17:01:41 -0700

    fixed seeds

--- backend/db/seeders/20230320215042-reviewimage.js
+++ backend/db/seeders/20230320215042-reviewimage.js
@@ -12,35 +12,35 @@ module.exports = {
       {
         // id: 1,
         reviewId: 1,
-        url: www.review.com,
+        url: 'www.review.com',
         // createdAt: new Date(),
         // updatedAt: new Date(),
       },
       {
         // id: 2,
         reviewId: 2,
-        url: www.review.com,
+        url: 'www.review.com',
         // createdAt: new Date(),
         // updatedAt: new Date(),
       },
       {
         // id: 3,
         reviewId: 3,
-        url: www.review.com,
+        url: 'www.review.com',
         // createdAt: new Date(),
         // updatedAt: new Date(),
       },
       {
         // id: 4,
         reviewId: 4,
-        url: www.review.com,
+        url: 'www.review.com',
         // createdAt: new Date(),
         // updatedAt: new Date(),
       },
       {
         // id: 5,
         reviewId: 5,
-        url: www.review.com,
+        url: 'www.review.com',
         // createdAt: new Date(),
         // updatedAt: new Date(),
       },
@@ -52,6 +52,7 @@ module.exports = {
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
       // username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+      reviewId: { [Op.in]: ['1', '2', '3'] }
     }, {});
   }
 };
