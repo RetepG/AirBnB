8cd001b5b4c622f13aa022b76f065fa8700729c9
Peter <guan_peter123@yahoo.com>
2023-03-21 22:24:58 -0700

    removed id

--- backend/db/seeders/20230320213512-spot.js
+++ backend/db/seeders/20230320213512-spot.js
@@ -90,7 +90,7 @@ module.exports = {
 
     return queryInterface.bulkDelete(options, {
       // username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
-      id: { [Op.in]: [1, 2, 3, 4, 5] }
+      // id: { [Op.in]: [1, 2, 3, 4, 5] }
     }, {});
   }
 };
