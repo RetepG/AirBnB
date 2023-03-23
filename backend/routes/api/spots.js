6699ca8510507759a7e56a99d45d5041fba4d69b
Peter <guan_peter123@yahoo.com>
2023-03-22 18:06:33 -0700

    created spot endpoint and fixed readme

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -6,6 +6,7 @@ const { Spot, Review, SpotImage, sequelize, User, ReviewImage } = require('../..
 
 const router = express.Router()
 
+//get all users
 router.get('/', async (req, res) => {
     // Find all spots and their associated data
     const spots = await Spot.findAll({
@@ -31,5 +32,58 @@ router.get('/', async (req, res) => {
     // Return all spots as the response
     return res.status(200).json({ Spots: spots });
 });
+
+
+
+// const validateSpot = [
+//     check('address')
+//         .exists({ checkFalsy: true })
+//         .withMessage('Street address is required'),
+//     check('city')
+//         .exists({ checkFalsy: true })
+//         .withMessage('City is required'),
+//     check('state')
+//         .exists({ checkFalsy: true })
+//         .withMessage('State is required'),
+//     check('country')
+//         .exists({ checkFalsy: true })
+//         .withMessage('Country is required'),
+//     check('lat')
+//         .exists({ checkFalsy: true })
+//         .withMessage('Latitude is not valid'),
+//     check('lng')
+//         .exists({ checkFalsy: true })
+//         .withMessage('Longitude is not valid'),
+//     check('name')
+//         .exists({ checkFalsy: true })
+//         .withMessage('Name must be less than 50 characters'),
+//     check('description')
+//         .exists({ checkFalsy: true })
+//         .withMessage('Description is required'),
+//     check('price')
+//         .exists({ checkFalsy: true })
+//         .withMessage('Price per day is required'),
+//     handleValidationErrorsSpots
+// ];
+
+// //create a spot
+// router.post('/', requireAuth, validateSpot, async (req, res) => {
+//     const { address, city, state, country, lat, lng, name, description, price } = req.body
+
+//     const createSpot = await Spot.create({
+//         ownerId: req.user.id,
+//         address,
+//         city,
+//         state,
+//         country,
+//         lat,
+//         lng,
+//         name,
+//         description,
+//         price,
+//     })
+
+//     return res.status(201).json(createSpot)
+// })
 
 module.exports = router;
