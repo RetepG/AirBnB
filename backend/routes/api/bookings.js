169be874e890af10fa1115baeeec9bce203abc6f
Peter <guan_peter123@yahoo.com>
2023-03-26 15:53:24 -0700

    done with booking put

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -60,23 +60,156 @@ router.get('/current', requireAuth, async (req, res) => {
     return res.status(200).json({ Bookings: bookingsArr })
 })
 
-const validBookTime = async (req, res, next) => {
-    const err = new Error('Validation error');
-    err.status = 400;
-    err.errors = {};
+// const validBookTime = async (req, res, next) => {
+//     const err = new Error('Validation error');
+//     err.status = 400;
+//     err.errors = {};
 
-    const startDate = new Date(req.body.startDate);
-    const endDate = new Date(req.body.endDate);
+//     const startDate = new Date(req.body.startDate);
+//     const endDate = new Date(req.body.endDate);
 
-    if (endDate <= startDate) {
-        err.errors.endDate = 'endDate cannot be on or before startDate'
-        return next(err);
+//     if (endDate <= startDate) {
+//         err.errors.endDate = 'endDate cannot be on or before startDate'
+//         return next(err);
+//     }
+//     next();
+// }
+
+// const checkBooking = async (req, res, next) => {
+//     const { bookingId } = req.params;
+//     const { startDate, endDate } = req.body;
+
+//     // Create new Date objects from startDate and endDate
+//     const startNew = new Date(startDate);
+//     const endNew = new Date(endDate);
+
+//     // Find all bookings for the specified spot and convert them to an array of JSON objects
+//     const bookings = await Booking.findAll({
+//         // where: {
+//         //     bookingId
+//         // },
+//         // attributes: ['id', 'spotId', 'userId', 'startDate', 'endDate', 'createdAt', 'updatedAt']
+//     });
+
+//     const bookingsArray = bookings.map((booking) => booking.toJSON());
+
+//     // Create an object to store the booking conflict error message and errors
+//     const bookingConflict = {
+//         message: "Sorry, this spot is already booked for the specified dates",
+//         errors: {
+//             // "startDate": "Start date conflicts with an existing booking",
+//             // "endDate": "End date conflicts with an existing booking"
+//         },
+//     };
+
+//     // Check if there is any booking that conflicts with the new booking
+//     const hasConflict = bookingsArray.some((booking) => { //checks array if one satisfies condition of true
+//         // return false
+//         // Create new Date objects from the current booking's start and end dates
+//         const currStart = new Date(booking.startDate);
+//         const currEnd = new Date(booking.endDate);
+
+//         // If the new booking start date falls within an existing booking period, set the error message and return true
+//         if (
+//             startNew.getTime() >= currStart.getTime() &&
+//             startNew.getTime() <= currEnd.getTime()
+//         ) {
+//             bookingConflict.errors.startDate =
+//                 "Start date conflicts with an existing booking";
+//             return true;
+//         }
+
+//         // If the new booking end date falls within an existing booking period, set the error message and return true
+//         if (
+//             endNew.getTime() >= currStart.getTime() &&
+//             endNew.getTime() <= currEnd.getTime()
+//         ) {
+//             bookingConflict.errors.endDate =
+//                 "End date conflicts with an existing booking";
+//             return true;
+//         }
+
+//         // If there is no conflict, return false
+//         return false;
+//     });
+
+//     // If there is a conflict, send a 403 response with the booking conflict error message and errors
+//     if (hasConflict) {
+//         return res.status(403).json(bookingConflict);
+//     }
+//     next();
+//     }
+
+//     //edit a booking based on booking id
+//     router.put('/:bookingId', requireAuth, validBookTime, checkBooking, async (req, res, next) => {
+//         const booking = await Booking.findByPk(req.params.bookingId)
+
+//         if (!booking) {
+//             // const err = new Error("Booking couldn't be found")
+//             // err.status = 404
+//             // return next(err)
+//             return res.status(404).json({ message: "Booking couldn't be found" })
+//         }
+
+//         if (booking.userId !== req.user.id) {
+//             // const err = new Error('Forbidden request')
+//             // err.status = 404
+//             return res.status(404).json({ message: "Forbidden request" })
+//         }
+
+//         const { startDate, endDate } = req.body
+
+//         let end = new Date(endDate)
+//         let now = new Date()
+
+//         if (now > end) {
+//             // const err = new Error("Past bookings can't be modified")
+//             // err.status = 400
+//             // return next(err)
+//             return res.status(400).json({ message: "Past bookings can't be modified" })
+//         }
+
+//         booking.set({ startDate, endDate })
+//         await booking.save()
+
+//         res.status(200).json(booking)
+//     })
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+router.put('/:bookingId', requireAuth, async (req, res, next) => {
+    const booking = await Booking.findByPk(req.params.bookingId)
+
+    if (!booking) {
+        // const err = new Error("Booking couldn't be found")
+        // err.status = 404
+        // return next(err)
+        return res.status(404).json({ message: "Booking couldn't be found" })
     }
-    next();
-}
 
-const checkBooking = async (req, res, next) => {
-    const { bookingId } = req.params;
+    if (booking.userId !== req.user.id) {
+        // const err = new Error('Forbidden request')
+        // err.status = 404
+        // return next(err)
+        return res.status(404).json({ message: "Forbidden request" })
+    }
+
+
+    const startDate2 = new Date(req.body.startDate);
+    const endDate2 = new Date(req.body.endDate);
+
+    // if (endDate <= startDate) {
+    //     return err.errors.endDate = 'endDate cannot be on or before startDate'
+    //     // return next(err);
+    // }
+    if (endDate2 <= startDate2) {
+        return res.status(400).json({
+            message: "Bad Request",
+            errors: {
+                "endDate": "endDate cannot come before startDate"
+            }
+        })
+    }
+
     const { startDate, endDate } = req.body;
 
     // Create new Date objects from startDate and endDate
@@ -97,8 +230,8 @@ const checkBooking = async (req, res, next) => {
     const bookingConflict = {
         message: "Sorry, this spot is already booked for the specified dates",
         errors: {
-            "startDate": "Start date conflicts with an existing booking",
-            "endDate": "End date conflicts with an existing booking"
+            // "startDate": "Start date conflicts with an existing booking",
+            // "endDate": "End date conflicts with an existing booking"
         },
     };
 
@@ -107,22 +240,36 @@ const checkBooking = async (req, res, next) => {
         // return false
         // Create new Date objects from the current booking's start and end dates
         const currStart = new Date(booking.startDate);
-        const currEnd = new Date(booking.end);
+        const currEnd = new Date(booking.endDate);
 
-        // If the new booking start date falls within an existing booking period, set the error message and return true
+        //If the new boooking start and end date with the existing booking period
         if (
-            startNew.getTime() >= currStart.getTime()  &&
-            startNew.getTime()  <= currEnd.getTime()
+            ((startNew.getTime() >= currStart.getTime() &&
+                startNew.getTime() <= currEnd.getTime()) &&
+                (endNew.getTime() >= currStart.getTime() &&
+                    endNew.getTime() <= currEnd.getTime()))
+        ) {
+            bookingConflict.errors.startDate =
+                "Start date conflicts with an existing booking";
+            bookingConflict.errors.endDate =
+                "End date conflicts with an existing booking";
+            return true;
+        }
+
+        // If the new booking start date falls within an existing booking period
+        if (
+            startNew.getTime() >= currStart.getTime() &&
+            startNew.getTime() <= currEnd.getTime()
         ) {
             bookingConflict.errors.startDate =
                 "Start date conflicts with an existing booking";
             return true;
         }
 
-        // If the new booking end date falls within an existing booking period, set the error message and return true
+        // If the new booking end date falls within an existing booking period
         if (
-            endNew.getTime()  >= currStart.getTime()  &&
-            endNew.getTime()  <= currEnd.getTime()
+            endNew.getTime() >= currStart.getTime() &&
+            endNew.getTime() <= currEnd.getTime()
         ) {
             bookingConflict.errors.endDate =
                 "End date conflicts with an existing booking";
@@ -135,28 +282,8 @@ const checkBooking = async (req, res, next) => {
 
     // If there is a conflict, send a 403 response with the booking conflict error message and errors
     if (hasConflict) {
-        return res.status(403).json(bookingConflict);
+        return res.status(403).json(bookingConflict)
     }
-    next();
-}
-
-//edit a booking based on booking id
-router.put('/:bookingId', requireAuth, validBookTime, checkBooking, async (req, res, next) => {
-    const booking = await Booking.findByPk(req.params.bookingId)
-
-    if (!booking) {
-        const err = new Error("Booking couldn't be found")
-        err.status = 404
-        return next(err)
-    }
-
-    if (booking.userId !== req.user.id) {
-        const err = new Error('Forbidden request')
-        err.status = 404
-        return next(err)
-    }
-
-    const { startDate, endDate } = req.body
 
     let end = new Date(endDate)
     let start = new Date()
@@ -172,7 +299,5 @@ router.put('/:bookingId', requireAuth, validBookTime, checkBooking, async (req, 
 
     res.status(200).json(booking)
 })
-
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 module.exports = router;
